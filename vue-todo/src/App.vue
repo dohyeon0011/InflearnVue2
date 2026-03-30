d669e55e65c4ad2d3bf02e00cd752715d33e12c8
dohyeon0011 <ehgus5026@naver.com>
2026-03-30 17:26:33 +0900

    Vuex 활용에 따른 리팩토링
    # 학습 내용
    - 기존 상위 컴포넌트인 App.vue에서 모든 기능을 모아 처리하는 방식에서 store에서 중간 처리

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+<!--    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>-->
+    <TodoInput></TodoInput>
     <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
     <TodoFooter v-on:clearItems="clearAllItems"></TodoFooter>
   </div>
@@ -14,48 +15,39 @@ import TodoList from './components/TodoList.vue';
 import TodoFooter from './components/TodoFooter.vue';
 
 export default {
-  data: function () {
+  data() {
     return {
       todoItems: []
     }
   },
-  methods: {
-    addOneItem: function (todoItem) {
-      // 하위 컴포넌트로부터 받은 이벤트(+값)을 기반으로 item을 저장하는 로직
-      var obj = {complete: false, item: todoItem};
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      this.todoItems.push(obj);
-    },
-    removeOneItem: function (todoItem, index) {
+  methods: {  // mutations
+    // addOneItem(todoItem) {
+    //   // 하위 컴포넌트로부터 받은 이벤트(+값)을 기반으로 item을 저장하는 로직
+    //   const obj = {complete: false, item: todoItem};
+    //   localStorage.setItem(todoItem, JSON.stringify(obj));
+    //   this.todoItems.push(obj);
+    // },
+    removeOneItem(todoItem, index) {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);  // slice()는 새로운 배열을 반환
     },
-    toggleOneItem: function (todoItem, index) {
+    toggleOneItem(todoItem, index) {
       // todoItem.completed = !todoItem.completed; // 자식 컴포넌트에게서 받은 데이터를 통짜로 받아서 값 수정하고 다시 내리는 거는 좋지 않은 패턴
       this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item); // update하는 API가 없어서 삭제 후, 변경된 completed 값으로 set
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItems: function () {
+    clearAllItems() {
       localStorage.clear();
       this.todoItems = [];
     }
   },
-  created: function () {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webapck-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-  },
   components: {
     // 컴포넌트 태그명 : 컴포넌트 내용
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
