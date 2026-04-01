7c7ce3a354cb0855ff2bf1f71c00ee53aab3da40
dohyeon0011 <ehgus5026@naver.com>
2026-04-01 17:22:50 +0900

    Vuex, ES6을 활용한 리팩토링
    # 학습 내용
    - 기존 상위 컴포넌트인 App.vue에서 모든 기능을 모아 처리하는 방식에서 store에서 중간 처리
    - getters, 스프레드 오퍼레이터 활용

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -22,25 +22,30 @@ export const store = new Vuex.Store({
     // headerText: 'TODO it'
     todoItems: storage.fetch()
   },
+  getters: {
+    storedTodoItems(state) {
+      return state.todoItems;
+    }
+  },
   mutations: {
     addOneItem(state, todoItem) {
       const obj = {complete: false, item: todoItem};
       localStorage.setItem(todoItem, JSON.stringify(obj));
       state.todoItems.push(obj);
     },
-    removeOneItem(state, index) {
-      localStorage.removeItem(state.item);
-      state.todoItems.splice(index, 1);  // slice()는 새로운 배열을 반환
+    removeOneItem(state, payload) {
+      localStorage.removeItem(payload.todoItem.item);
+      state.todoItems.splice(payload.index, 1);  // slice()는 새로운 배열을 반환
     },
-    toggleOneItem(state, index) {
+    toggleOneItem(state, payload) {
       // todoItem.completed = !todoItem.completed; // 자식 컴포넌트에게서 받은 데이터를 통짜로 받아서 값 수정하고 다시 내리는 거는 좋지 않은 패턴
-      state.todoItems[index].completed = !state.todoItems[index].completed;
-      localStorage.removeItem(state.item); // update하는 API가 없어서 삭제 후, 변경된 completed 값으로 set
-      localStorage.setItem(state.item, JSON.stringify(state));
+      state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+      localStorage.removeItem(payload.todoItem.item); // update하는 API가 없어서 삭제 후, 변경된 completed 값으로 set
+      localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
     },
-    clearAllItems() {
+    clearAllItems(state) {
       localStorage.clear();
-      this.state.todoItems = [];
+      state.todoItems = [];
     }
   }
 });
